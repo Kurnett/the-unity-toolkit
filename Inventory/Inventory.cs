dbd8ed4c778b38647bd3325f80d6332fad84f45a
John Kurnett <john@kurnett.com>
2020-03-22 13:33:46 -0700

    Moved Inventory to inheritance model.

--- Inventory/Inventory.cs
+++ Inventory/Inventory.cs
@@ -2,90 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
-
-  Inheritance-based inventory
-  Base
-  - Items
-  - Picking up items
-  - Dropping items
-
-    Equipping
-    - Item positions
-
-*/
-
 public class Inventory : MonoBehaviour {
 
-  public bool canPickUpItems = true;
-  public bool autoHoldItems = false;
-  public string autoHoldPositionName = "";
-  ItemPosition autoHoldPosition;
+  protected List<Item> inventory = new List<Item>();
 
-  public List<Item> inventory = new List<Item>();
-  public List<ItemPosition> itemPositions = new List<ItemPosition>();
-
-  void Start() {
-    autoHoldPosition = FindPositionByName(autoHoldPositionName);
-  }
-
-  public Item GetItem(string name) {
+  virtual public Item GetItem(string name) {
     return FindItemInInventory(name);
   }
 
-  public Item GetItem(Item item) {
+  virtual public Item GetItem(Item item) {
     return FindItemInInventory(item);
   }
 
-  public void AddItem(Item item) {
-    if (canPickUpItems) {
-      AddToInventory(item);
-    }
+  virtual public void AddItem(Item item) {
+    AddToInventory(item);
   }
 
-  public void AddItem(Item item, GameObject prop) {
-    if (canPickUpItems) {
-      AddToInventory(item);
-      Destroy(prop);
-    }
+  virtual public void AddItem(Item item, GameObject prop) {
+    AddToInventory(item);
+    Destroy(prop);
   }
 
-  void AddToInventory(Item item) {
+  virtual protected void AddToInventory(Item item) {
     inventory.Add(item);
-    if (autoHoldItems && autoHoldPosition != null) {
-      autoHoldPosition.SetItem(item);
-    }
   }
 
-  public void DropItem(Item item) {
-    if (item != null) {
-      GameObject.Instantiate(item.prop, transform.position, transform.rotation);
-      RemoveItem(item);
-    }
-  }
-
-  public void DropItem(string name) {
-    Item item = FindItemInInventory(name);
-    DropItem(item);
-  }
-
-  public void DropItemByPosition(string name) {
-    ItemPosition position = FindPositionByName(name);
-    Item item = position.currentItem ? position.currentItem : null;
-    DropItem(item);
-  }
-
-  public void RemoveItem(string name) {
+  virtual public void RemoveItem(string name) {
     Item item = FindItemInInventory(name);
     RemoveItem(item);
   }
 
-  public void RemoveItem(Item item) {
+  virtual public void RemoveItem(Item item) {
     inventory.Remove(item);
-    RemoveItemFromPosition(item);
   }
 
-  Item FindItemInInventory(string name) {
+  virtual protected Item FindItemInInventory(string name) {
     foreach (Item item in inventory) {
       if (item.name == name) {
         return item;
@@ -94,33 +45,13 @@ public class Inventory : MonoBehaviour {
     return null;
   }
 
-  Item FindItemInInventory(Item itemToFind) {
+  virtual protected Item FindItemInInventory(Item itemToFind) {
     foreach (Item item in inventory) {
       if (item == itemToFind) {
         return item;
       }
     }
     return null;
-  }
-
-  ItemPosition FindPositionByName(string name) {
-    foreach (ItemPosition position in itemPositions) {
-      if (position.name == name) {
-        return position;
-      }
-    }
-    return null;
-  }
-
-  bool RemoveItemFromPosition(Item item) {
-    foreach (ItemPosition position in itemPositions) {
-      if (position.currentItem == item) {
-        GameObject.Destroy(position.currentItemProp);
-        position.currentItem = null;
-        return true;
-      }
-    }
-    return false;
   }
 
 }
--- Inventory/Item.cs
+++ Inventory/Item.cs
@@ -2,30 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Items/Items")]
+[CreateAssetMenu(menuName = "Items/Item")]
 public class Item : ScriptableObject {
-  public bool unique;
   new public string name;
-  public string description;
-  public GameObject prop;
-  public GameObject heldProp;
-}
-
-[System.Serializable]
-public class ItemPosition {
-  public string name;
-  public Transform position;
-  public Item currentItem;
-  public GameObject currentItemProp;
-
-  public ItemPosition(string nameInit, Transform positionInit) {
-    name = nameInit;
-    position = positionInit;
-  }
-
-  public void SetItem(Item item) {
-    currentItem = item;
-    currentItemProp = GameObject.Instantiate(item.heldProp, position);
-    currentItemProp.transform.localPosition = Vector3.zero;
-  }
 }
