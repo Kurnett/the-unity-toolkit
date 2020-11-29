4b69d1902d5e441e06e9db97a431b783a72ff162
John Kurnett <john@kurnett.com>
2020-11-29 14:02:30 -0800

    Added initial flag system.

---
+++ Flags/FlagManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagManager : MonoBehaviour {
+  [SerializeField]
+  private List<Flag> activeFlags = new List<Flag>();
+
+  public void SetFlag(Flag flag) {
+    if (!activeFlags.Contains(flag)) {
+      activeFlags.Add(flag);
+    }
+  }
+
+  public void RemoveFlag(Flag flag) {
+    if (activeFlags.Contains(flag)) {
+      activeFlags.Remove(flag);
+    }
+  }
+
+  public bool HasFlag(Flag flag) {
+    return activeFlags.Contains(flag);
+  }
+}
